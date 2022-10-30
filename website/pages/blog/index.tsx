572194167a1d49e3fd672ec143ea05cb4a3ca413
Edgar Perez <edgarp.dev@gmail.com>
2022-10-30 13:24:18 -0600

    [UPDATE] Added missing Head Tags

--- website/pages/blog/index.tsx
+++ website/pages/blog/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import Head from 'next/head';
 import { BlogPost } from '../../@types/schema';
 import PostItem from '../../src/components/PostItem';
 import padding from '../../src/utils/padding';
@@ -22,11 +23,28 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
 
 const Blog = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
-    <main className={`flex flex-col flex-grow ${padding.generalPadding}`}>
-      {posts.map((post) => {
-        return <PostItem key={post.id} post={post} />;
-      })}
-    </main>
+    <>
+      <Head>
+        <title>Blog</title>
+        <meta name={'og:title'} title={'og:title'} content="Blog" />
+        <meta name={'og:type'} title={'og:type'} content={'website'} />
+        <meta
+          name={'description'}
+          title={'description'}
+          content="edgarp.dev blog"
+        />
+        <meta
+          name={'og:description'}
+          title={'og:description'}
+          content="edgarp.dev blog"
+        />
+      </Head>
+      <main className={`flex flex-col flex-grow ${padding.generalPadding}`}>
+        {posts.map((post) => {
+          return <PostItem key={post.id} post={post} />;
+        })}
+      </main>
+    </>
   );
 };
 
