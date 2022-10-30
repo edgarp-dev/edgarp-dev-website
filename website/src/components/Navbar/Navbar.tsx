5a5665418b18dea5e975c2347c938964ada59f36
Edgar Perez <edgarp.dev@gmail.com>
2022-10-30 12:59:10 -0600

    [UPDATE] Removed colors from colors file and updates styles

--- website/src/components/Navbar/Navbar.tsx
+++ website/src/components/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ const NavBar = () => {
   return (
     <nav
       id="header"
-      className={`fixed min-w-full z-10 top-0 h-14 decoration-solid bg-[${colors.secondaryColor}] shadow-lg`}>
+      className="fixed min-w-full z-10 top-0 h-14 decoration-solid bg-[#222831] shadow-lg">
       <div
         className={`mx-auto md:max-w-3xl ${padding.generalPadding} flex flex-wrap items-center justify-between mt-0 py-3`}>
         <span
--- website/src/utils/colors.ts
+++ website/src/utils/colors.ts
@@ -1,7 +1,5 @@
 const colors = {
   textColor: 'text-slate-200',
-  primaryColor: '#393E46',
-  secondaryColor: '#222831',
 };
 
 export default colors;
