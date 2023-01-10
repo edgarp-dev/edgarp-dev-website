dfd5f0f43c0e2829a6c5bee63ce0142426ad6632
Edgar Perez <edgarp.dev@gmail.com>
2023-01-10 08:53:33 -0600

    [UPDATE] Retrieve blog path urls and added env variables file to test locally

--- revalidate-lambda-function/revalidate-function/src/app.js
+++ revalidate-lambda-function/revalidate-function/src/app.js
@@ -1,15 +1,41 @@
 import axios from 'axios';
+import { Client } from '@notionhq/client';
+
+const notionClient = new Client({
+    auth: process.env.NOTION_ACCESS_TOKEN
+});
 
 export const lambdaHandler = async (event, context) => {
     try {
-        // console.log('>>>> REVALIDATING');
+        console.log('>>>> REVALIDATING');
 
+        const blogsDatabaseResponse = await notionClient.databases.query({
+            database_id: process.env.NOTION_BLOG_DATABASE_ID || '',
+            filter: {
+                property: 'Published',
+                checkbox: {
+                    equals: true,
+                },
+            },
+            sorts: [
+                {
+                    property: 'Date',
+                    direction: 'ascending',
+                },
+            ],
+        });
+
+        const blogPostsUrlPaths = blogsDatabaseResponse.results.map(blogDatabaseItem => {
+            return blogDatabaseItem.properties.Slug.rich_text[0].plain_text;
+        });
+
+        console.log(blogPostsUrlPaths)
         // const revalidateUrl = `https://www.edgarp.dev/api/revalidate?secret=${process.env.REVALIDATE_TOKEN}`;
         // console.log(revalidateUrl);
         // const response = await axios.get(revalidateUrl);
         // console.log(response.data);
 
-        // console.log('>>>> FINISHED REVALIDATING');
+        console.log('>>>> FINISHED REVALIDATING');
         return {
             statusCode: 200,
             body: JSON.stringify({
@@ -17,7 +43,7 @@ export const lambdaHandler = async (event, context) => {
             })
         };
     } catch (err) {
-        console.log(err.message);
+        console.errorcd(err.message);
         return {
             statusCode: 500,
             body: JSON.stringify({
